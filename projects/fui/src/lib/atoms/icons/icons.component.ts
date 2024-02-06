ad3d3417192d3dafcb18973a19d0271289c80ea5
noval232 <adilaga.noval@gmail.com>
2024-02-06 13:16:17 +0700

    add icons component

---
+++ projects/fui/src/lib/atoms/icons/icons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fui-icons',
+  standalone: true,
+  imports: [],
+  templateUrl: './icons.component.html',
+  styleUrl: './icons.component.scss'
+})
+export class IconsComponent {
+
+}
