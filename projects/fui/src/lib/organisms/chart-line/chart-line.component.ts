380d79ec9ae2e75a387f73a2eb76a36d99103890
desukyu <desukyu@gmail.com>
2024-02-06 13:56:24 +0700

    create fui chart line component

---
+++ projects/fui/src/lib/organisms/chart-line/chart-line.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fui-chart-line',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart-line.component.html',
+  styleUrl: './chart-line.component.scss'
+})
+export class ChartLineComponent {
+
+}
