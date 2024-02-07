f70c33333ae01a5f4a0a318989b396bd89fbc59f
noval232 <adilaga.noval@gmail.com>
2024-02-07 15:06:26 +0700

    change fui-input to fui-input-base and removed as in use

--- projects/fui/src/lib/atoms/input/input.component.ts
+++ projects/fui/src/lib/atoms/input/input.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'fui-field-text',
+  selector: 'fui-input-base',
   standalone: true,
   imports: [],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
 })
 export class InputComponent {
-  @Input('type') typeValue?: string;
-  @Input('label') textValue?: string;
-  @Input('placeholder') placeholderValue?: string;
-  @Input('helpText') helpTextValue?: string;
+  // @Input('type') typeValue?: string;
+  // @Input('label') textValue?: string;
+  // @Input('helpText') helpTextValue?: string;
+  @Input('placeholder') placeholderValue = '';
+  @Input('color') colorValue = 'primary';
+  @Input('size') sizeValue: string = 'sizem';
+  @Input('width') widthValue: string = 'auto';
 }
