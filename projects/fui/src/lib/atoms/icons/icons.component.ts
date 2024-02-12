203c24919654be1ba7ac5b451da6bfd5d0bbfb27
laska.amrullah <laska.amrullah@ahp.id>
2024-02-12 17:20:37 +0700

    feat: component atom icon

--- projects/fui/src/lib/atoms/icons/icons.component.ts
+++ projects/fui/src/lib/atoms/icons/icons.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Color, Icon, Size } from '../../types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'fui-icons',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './icons.component.html',
-  styleUrl: './icons.component.scss'
+  styleUrl: './icons.component.scss',
 })
 export class IconsComponent {
-
+  @Input() icon!: Icon;
+  @Input() size: Size = 'sizedefault';
+  @Input() color!: Color;
 }
