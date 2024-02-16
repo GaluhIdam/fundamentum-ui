97dc6a9e02f4d430b0eef9f9e4d0b422d4c558ce
laska.amrullah <laska.amrullah@ahp.id>
2024-02-16 09:58:08 +0700

    feat: molecule accrodion

---
+++ projects/fui/src/lib/molecules/accordion/accordion.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Color, Icon, Size } from '../../types';
+import { IconsComponent } from '../../atoms/icons/icons.component';
+import { TextComponent } from '../../atoms/text/text.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'fui-accordion',
+  standalone: true,
+  templateUrl: './accordion.component.html',
+  styleUrl: './accordion.component.scss',
+  imports: [CommonModule, IconsComponent, TextComponent],
+})
+export class AccordionComponent {
+  @Input() isExpand: boolean = false;
+  @Input() title: string = '';
+  @Input() titleSize: Size = 'sizedefault';
+  @Input() titleColor!: Color;
+  @Input() bodySize: Size = 'sizedefault';
+  @Input() bodyColor!: Color;
+  @Input() iconSize: Size = 'sizedefault';
+  @Input() iconColor!: Color;
+  @Input() collapsedIcon: Icon = 'arrowRight';
+  @Input() expandIcon: Icon = 'arrowDown';
+  @Input() isLoading: boolean = false;
+  @Input() expandIconPosition: 'start' | 'end' = 'start';
+  @Output() onExpand: EventEmitter<void> = new EventEmitter();
+
+  onExpandAccordion() {
+    this.isExpand = !this.isExpand;
+    this.onExpand.emit();
+  }
+}
