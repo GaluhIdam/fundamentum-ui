74c4ad3dd83fdb4778d2a370b58b1224d32e7c1c
laska.amrullah <laska.amrullah@ahp.id>
2024-02-19 17:46:56 +0700

    feat: routing demo template modal

--- projects/demo/src/app/app.routes.ts
+++ projects/demo/src/app/app.routes.ts
@@ -10,6 +10,7 @@ import { ChartAreaStackedViewComponent } from './organisms/chart-area-stacked-vi
 import { ChartLineStackedViewComponent } from './organisms/chart-line-stacked-view/chart-line-stacked-view.component';
 import { ChartAreaStackedGradientViewComponent } from './organisms/chart-area-stacked-gradient-view/chart-area-stacked-gradient-view.component';
 import { ChartPieDoughnutViewComponent } from './organisms/chart-pie-doughnut-view/chart-pie-doughnut-view.component';
+import { ModalViewComponent } from './templates/modal-view/modal-view.component';
 
 export const routes: Routes = [
   {
@@ -75,6 +76,15 @@ export const routes: Routes = [
         title: 'Chart Area Stacked Gradient',
         path: 'chart-area-stacked-gradient',
         component: ChartAreaStackedGradientViewComponent,
+      },
+    ],
+  },
+  {
+    path: 'template',
+    children: [
+      {
+        path: 'modal',
+        component: ModalViewComponent,
       },
     ],
   },
