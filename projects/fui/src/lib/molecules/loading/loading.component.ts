cff58396ae57dd2e4ee23cbc2fc0df915d0c0609
laska.amrullah <laska.amrullah@ahp.id>
2024-03-09 09:54:24 +0700

    feat: add input withOverlay and isLoading in Loading component

--- projects/fui/src/lib/molecules/loading/loading.component.ts
+++ projects/fui/src/lib/molecules/loading/loading.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Size } from '../../types';
 import { CommonModule } from '@angular/common';
+import { OverlayMaskComponent } from '../../atoms/overlay-mask/overlay-mask.component';
 
 @Component({
   selector: 'fui-loading',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './loading.component.html',
   styleUrl: './loading.component.scss',
+  imports: [CommonModule, OverlayMaskComponent],
 })
 export class LoadingComponent {
+  @Input() withOverlay: boolean = false;
+  @Input() isLoading: boolean = false;
   @Input() type: 'elastic' | 'logo' | 'chart' | 'spinner' = 'spinner';
   @Input() size: Size = 'sizedefault';
   @Input() logoImage: string = '';
