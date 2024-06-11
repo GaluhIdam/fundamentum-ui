18cf7830c2a1e0b91ad2f1b3cd06f02efe0b1185
desukyu <desukyu@gmail.com>
2024-06-11 15:43:19 +0700

    update date-range component

--- projects/fui/src/lib/organisms/date-range/date-range.component.ts
+++ projects/fui/src/lib/organisms/date-range/date-range.component.ts
@@ -10,6 +10,7 @@ import {FlexGroupComponent} from "../../templates/flex/flex-group.component";
 import {InputFieldComponent} from "../../molecules/form-control-layout/input-field/input-field.component";
 import {NgForOf} from "@angular/common";
 import {PopoverComponent} from "../../templates/popover/popover.component";
+import {b} from "vite/dist/node/types.d-aGj9QkWt";
 
 @Component({
   selector: 'fui-date-range',
@@ -37,112 +38,170 @@ export class DateRangeComponent {
   @Input() minuteInterval: MinuteInterval = "30 minutes";
   @Input() showTimeOptions: boolean = false;
 
-  @Input() isInvalid: boolean = false;
-  @Output() isInvalidChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Input() isInvalidStartDate: boolean = false;
+  @Output() isInvalidStartDateChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  @Output() onChange: EventEmitter<string> = new EventEmitter<string>();
+  @Input() isInvalidEndDate: boolean = false;
+  @Output() isInvalidEndDateChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  @Output() onChangeStartDate: EventEmitter<string> = new EventEmitter<string>();
+  @Output() onChangeEndDate: EventEmitter<string> = new EventEmitter<string>();
 
   placeholder: string = "";
   days: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   years: number[] = [];
-  weeks: Array<Array<{date: dayjs.Dayjs, selected: boolean}>> = [];
+  weeks: Array<Array<{date: dayjs.Dayjs, selected: boolean, betweenRange: boolean}>> = [];
   timeOptions: Array<{time: dayjs.Dayjs, selected: boolean}> = [];
-  currentDate: dayjs.Dayjs;
 
-  displayMonth: dayjs.Dayjs;
+  displayMonth: dayjs.Dayjs | null = null;
 
-  selectedDate: dayjs.Dayjs | null = null;
-  selectedMonth: number;
-  selectedYear: number;
-  selectedTime: dayjs.Dayjs | null = null;
-
+  selectedStartDate: dayjs.Dayjs | null = null;
+  selectedEndDate: dayjs.Dayjs | null = null;
+  selectedStartMonth: number | null = null;
+  selectedEndMonth: number | null = null;
+  selectedStartYear: number | null = null;
+  selectedEndYear: number | null = null;
+  selectedStartTime: dayjs.Dayjs | null = null;
+  selectedEndTime: dayjs.Dayjs | null = null;
 
   constructor() {
-    this.currentDate = dayjs();
-    this.displayMonth = dayjs().startOf('month');
-    this.selectedMonth = dayjs().month() + 1;
-    this.selectedYear = dayjs().year();
     this.years = Array.from({ length: 15 }, (_, i) => dayjs().year() - 7 + i);
   }
 
   ngOnInit(): void {
-    this.placeholder = this.dateFormat
+    this.placeholder = this.dateFormat;
     if (this.showTimeOptions) {
       this.placeholder = this.placeholder + ' ' + (this.timeFormat === "12h"? 'HH:MM AM': 'HH:MM');
     }
-    this.generateCalendar(this.displayMonth);
   }
 
-  isDateSelected(date: dayjs.Dayjs): boolean {
-    return this.selectedDate ? date.isSame(this.selectedDate, 'day'): false;
+  isDateSelected(date: dayjs.Dayjs, selectedDate: dayjs.Dayjs | null): boolean {
+    if (selectedDate === null) {
+      return false;
+    }
+    return date.isSame(selectedDate, 'day');
   }
 
-  isTimeSelected(time: dayjs.Dayjs): boolean {
-    if (!this.selectedTime) {
+  isBetweenRange(date: dayjs.Dayjs, selectedStartDate: dayjs.Dayjs | null, selectedEndDate: dayjs.Dayjs | null): boolean {
+    if (selectedStartDate === null) {
+      return false;
+    }
+    if (selectedEndDate === null) {
+      return false;
+    }
+    return date.isAfter(selectedStartDate, 'day') && date.isBefore(selectedEndDate, 'day');
+  }
+
+  isTimeSelected(time: dayjs.Dayjs, selectedTime: dayjs.Dayjs | null): boolean {
+    if (selectedTime === null) {
       return false;
     }
     let interval: number = this.generateMinuteInterval();
-    const roundedMinute: number = Math.trunc(this.selectedTime.minute() / interval) * interval;
-    const isSameHour = time.hour() === this.selectedTime.hour();
+
+    const roundedMinute: number = Math.trunc(selectedTime.minute() / interval) * interval;
+    const isSameHour = time.hour() === selectedTime.hour();
     const isSameMinute = time.minute() === roundedMinute;
     return isSameHour && isSameMinute;
   }
 
-  generateCalendar(month: dayjs.Dayjs): void {
+  generateCalendar(month: dayjs.Dayjs, isStartDate: boolean): void {
     this.weeks = [];
     const startOfMonth = month.startOf('month');
     const endOfMonth = month.endOf('month');
     let startDate = startOfMonth.startOf('week');
 
     while (startDate.isBefore(endOfMonth.endOf('week'))) {
-      const week: Array<{date: dayjs.Dayjs, selected: boolean}> = [];
+      const week: Array<{date: dayjs.Dayjs, selected: boolean, betweenRange: boolean}> = [];
       for (let i = 0; i < 7; i++) {
-        week.push({date: startDate, selected: this.isDateSelected(startDate)});
+        week.push({date: startDate
+          , selected: this.isDateSelected(startDate, this.selectedStartDate) || this.isDateSelected(startDate, this.selectedEndDate)
+          , betweenRange: this.isBetweenRange(startDate, this.selectedStartDate, this.selectedEndDate)});
         startDate = startDate.add(1, 'day');
       }
       this.weeks.push(week);
     }
 
-    this.selectedMonth = month.month();
-    this.selectedYear = month.year();
+    if (isStartDate) {
+      this.selectedStartMonth = month.month();
+      this.selectedStartYear = month.year();
+    } else {
+      this.selectedEndMonth = month.month();
+      this.selectedEndYear = month.year();
+    }
+
     this.years = Array.from({ length: 15 }, (_, i) => month.year() - 7 + i);
 
-    this.generateTimeOptions();
+    this.generateTimeOptions(isStartDate);
   }
 
-  selectDate(date: dayjs.Dayjs): void {
-    this.selectedDate = date;
-    this.generateCalendar(this.displayMonth);
+  selectDate(date: dayjs.Dayjs, isStartDate: boolean): void {
+    if (isStartDate) {
+      this.selectedStartDate = date;
+    } else {
+      this.selectedEndDate = date;
+    }
+    this.displayMonth = date;
+    this.generateCalendar(this.displayMonth, isStartDate);
     if (!this.showTimeOptions) {
-      // this.dateFormControl = new FormControl(this.formatDate(this.selectedDate));
-      this.isInvalid = false;
-      this.isInvalidChange.emit(this.isInvalid);
-      // this.onChange.emit(this.dateFormControl.value);
+      if (isStartDate) {
+        if (this.selectedStartDate) {
+          this.startDateControl = new FormControl(this.formatDate(this.selectedStartDate));
+          this.isInvalidStartDate = false;
+          this.isInvalidStartDateChange.emit(this.isInvalidStartDate);
+          this.onChangeStartDate.emit(this.startDateControl.value);
+        }
+      } else{
+        if (this.selectedEndDate) {
+          this.endDateControl = new FormControl(this.formatDate(this.selectedEndDate));
+          this.isInvalidEndDate = false;
+          this.isInvalidEndDateChange.emit(this.isInvalidEndDate);
+          this.onChangeEndDate.emit(this.endDateControl.value);
+        }
+      }
+
     }
   }
 
-  selectTime(time: dayjs.Dayjs): void {
-    this.selectedTime = time;
-    if (this.selectedDate) {
-      this.generateTimeOptions();
-      const date: string = this.formatDate(this.selectedDate);
-      const time: string = this.formatTime(this.selectedTime);
-      // this.dateFormControl = new FormControl(date + " " + time);
-      this.isInvalid = false;
-      this.isInvalidChange.emit(this.isInvalid);
-      // this.onChange.emit(this.dateFormControl.value);
+  selectTime(time: dayjs.Dayjs, isStartDate: boolean): void {
+    this.generateTimeOptions(isStartDate);
+    if (isStartDate) {
+      this.selectedStartTime = time;
+
+      if (this.selectedStartDate) {
+        const date: string = this.formatDate(this.selectedStartDate);
+        const time: string = this.formatTime(this.selectedStartTime);
+        this.startDateControl = new FormControl(date + " " + time);
+        this.isInvalidStartDate = false;
+        this.isInvalidStartDateChange.emit(this.isInvalidStartDate);
+        this.onChangeStartDate.emit(this.startDateControl.value);
+      }
+    } else {
+      this.selectedEndTime = time;
+
+      if (this.selectedEndDate) {
+        const date: string = this.formatDate(this.selectedEndDate);
+        const time: string = this.formatTime(this.selectedEndTime);
+        this.endDateControl = new FormControl(date + " " + time);
+        this.isInvalidEndDate = false;
+        this.isInvalidEndDateChange.emit(this.isInvalidEndDate);
+        this.onChangeEndDate.emit(this.endDateControl.value);
+      }
     }
   }
 
-  prevMonth(): void {
-    this.displayMonth = this.displayMonth.subtract(1, 'month');
-    this.generateCalendar(this.displayMonth);
+  prevMonth(isStartDate: boolean): void {
+    if (this.displayMonth) {
+      this.displayMonth = this.displayMonth.subtract(1, 'month');
+      this.generateCalendar(this.displayMonth, isStartDate);
+    }
   }
 
-  nextMonth(): void {
-    this.displayMonth = this.displayMonth.add(1, 'month');
-    this.generateCalendar(this.displayMonth);
+  nextMonth(isStartDate: boolean): void {
+    if (this.displayMonth) {
+      this.displayMonth = this.displayMonth.add(1, 'month');
+      this.generateCalendar(this.displayMonth, isStartDate);
+    }
   }
 
   formatDate(date: dayjs.Dayjs): string {
@@ -157,33 +216,65 @@ export class DateRangeComponent {
     }
   }
 
-  changeMonth(month: any): void {
-    this.selectedMonth = month;
-    this.displayMonth = this.displayMonth.month(this.selectedMonth);
-    this.generateCalendar(this.displayMonth);
+  changeMonth(month: any, isStartDate: boolean): void {
+    if (!this.displayMonth) {
+      return;
+    }
+    if (isStartDate) {
+      this.selectedStartMonth = month;
+    } else {
+      this.selectedEndMonth = month;
+    }
+
+    this.displayMonth = this.displayMonth.month(month);
+    this.generateCalendar(this.displayMonth, isStartDate);
   }
 
-  changeYear(year: any): void {
-    this.selectedYear = year;
-    this.displayMonth = this.displayMonth.year(this.selectedYear);
-    this.generateCalendar(this.displayMonth);
+  changeYear(year: any, isStartDate: boolean): void {
+    if (!this.displayMonth) {
+      return;
+    }
+    if (isStartDate) {
+      this.selectedStartYear = year;
+    } else {
+      this.selectedEndYear = year;
+    }
+
+    this.displayMonth = this.displayMonth.year(year);
+    this.generateCalendar(this.displayMonth, isStartDate);
   }
 
-  changeTextInput(event: any): void {
+  changeTextInput(event: any, isStartDate: boolean): void {
     const inputValue = event.target.value;
-    this.isInvalid = !this.validateDateInput(inputValue);
-    this.isInvalidChange.emit(this.isInvalid);
-    if (!this.isInvalid) {
-      let format: string = this.dateFormat;
-      if (this.showTimeOptions) {
-        format = format +  ' ' + (this.timeFormat === "12h"? 'hh:mm A': 'HH:mm');
+    let format: string = this.dateFormat;
+    if (this.showTimeOptions) {
+      format = format +  ' ' + (this.timeFormat === "12h"? 'hh:mm A': 'HH:mm');
+    }
+
+    if (isStartDate) {
+      this.isInvalidStartDate = !this.validateDateInput(inputValue);
+      this.isInvalidStartDateChange.emit(this.isInvalidStartDate);
+      if (!this.isInvalidStartDate) {
+        this.selectedStartDate = dayjs(inputValue, format, true);
+        if (this.showTimeOptions) {
+          this.selectedStartTime = this.selectedStartDate;
+        }
+        this.onChangeStartDate.emit(this.startDateControl.value);
       }
-      this.selectedDate = dayjs(inputValue, format, true);
-      if (this.showTimeOptions) {
-        this.selectedTime = this.selectedDate;
+    } else {
+      this.isInvalidEndDate = !this.validateDateInput(inputValue);
+      this.isInvalidEndDateChange.emit(this.isInvalidEndDate);
+      if (!this.isInvalidEndDate) {
+        this.selectedEndDate = dayjs(inputValue, format, true);
+        if (this.showTimeOptions) {
+          this.selectedEndTime = this.selectedEndDate;
+        }
+        this.onChangeEndDate.emit(this.endDateControl.value);
       }
-      this.generateCalendar(this.displayMonth);
-      // this.onChange.emit(this.dateFormControl.value);
+    }
+
+    if (this.displayMonth) {
+      this.generateCalendar(this.displayMonth, isStartDate);
     }
   }
 
@@ -196,15 +287,19 @@ export class DateRangeComponent {
     return dayjs(inputValue, format, true).isValid();
   }
 
-  generateTimeOptions(): void {
+  generateTimeOptions(isStartDate: boolean): void {
     if (this.showTimeOptions) {
+      let selectedTime = this.selectedEndTime;
+      if (isStartDate) {
+        selectedTime = this.selectedStartTime;
+      }
       this.timeOptions = [];
       let interval: number = this.generateMinuteInterval();
       const minutesSize = 60 / interval;
       for (let i = 0; i < 24; i++) {
         for (let j = 0; j < minutesSize; j++) {
           const timeOption = dayjs().hour(i).minute(j * interval);
-          this.timeOptions.push({time: timeOption, selected: this.isTimeSelected(timeOption)});
+          this.timeOptions.push({time: timeOption, selected: this.isTimeSelected(timeOption, selectedTime)});
         }
       }
     }
@@ -225,4 +320,25 @@ export class DateRangeComponent {
     return interval;
   }
 
+  onStartDateClick() {
+    if (this.selectedStartDate) {
+      this.displayMonth = this.selectedStartDate;
+    } else {
+      this.displayMonth = dayjs();
+    }
+    this.selectedStartMonth = this.displayMonth.month();
+    this.selectedStartYear = this.displayMonth.year();
+    this.generateCalendar(this.displayMonth, true);
+  }
+
+  onEndDateClick() {
+    if (this.selectedEndDate) {
+      this.displayMonth = this.selectedEndDate;
+    } else {
+      this.displayMonth = dayjs();
+    }
+    this.selectedEndMonth = this.displayMonth.month();
+    this.selectedEndYear = this.displayMonth.year();
+    this.generateCalendar(this.displayMonth, false);
+  }
 }
